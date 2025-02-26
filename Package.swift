756b3a2acec62bb0dc3e05b354c445994d83e052
Girish J <jgirish123@gmail.com>
2025-02-25 23:46:16 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Transcend",
-            url: "https://cdn.transcend.io/consent-manager-ios-library/releases/download/1.1.10/Transcend.xcframework.zip",
-            checksum: "8d8a21c3403da1c3220dd95aeed9bbe0c7a44fc890df8b4df2378d307e134ec4"
+            url: "https://cdn.transcend.io/consent-manager-ios-library/releases/download/1.1.11/Transcend.xcframework.zip",
+            checksum: "f7b2c11f502ae8a4f5a191af52382afa40bc705135354adb9e211797b4b88e5b"
         ),
     ]
 )
